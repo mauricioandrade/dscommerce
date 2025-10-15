4311823df8ed74b2574c65aeb12d6dfe29487d66
mauricioandrade <mauricio_andrade@outlook.com>
2025-10-15 11:14:33 -0300

    feat: add OrderItem e OrderItemPK

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -13,10 +13,15 @@ public class User {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+    @Column(unique = true)
     private String email;
+
     private String phone;
+
     private LocalDate birthDate;
+
     private String password;
 
     public User() {
