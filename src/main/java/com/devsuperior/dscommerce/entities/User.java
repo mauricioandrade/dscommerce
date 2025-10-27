c4ad3a2d9af564af58e086bedaad2d392a3af92e
mauricioandrade <mauricio_andrade@outlook.com>
2025-10-27 13:36:03 -0300

    feat: add Role model

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -3,16 +3,14 @@ package com.devsuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
-@Table (name = "tb_user")
+@Table(name = "tb_user")
 public class User {
 
     @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
@@ -24,6 +22,10 @@ public class User {
     private LocalDate birthDate;
 
     private String password;
+
+    @ManyToMany
+    @JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     public User() {
     }
@@ -90,6 +92,19 @@ public class User {
 
     public List<Order> getOrders() {
         return orders;
+    }
+
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public boolean hasRole(String roleName) {
+        for (Role role : roles) {
+            if (role.getAuthority().equals(roleName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
