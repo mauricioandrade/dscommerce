ec94891e628fd716033adf700a6703b88873bd42
mauricioandrade <mauricio_andrade@outlook.com>
2025-11-11 20:48:06 -0300

    GET Categories

--- src/main/java/com/devsuperior/dscommerce/repositories/CategoryRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/CategoryRepository.java
@@ -1,9 +1,12 @@
 package com.devsuperior.dscommerce.repositories;
 
-import com.devsuperior.dscommerce.entities.Order;
+import com.devsuperior.dscommerce.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
 }
